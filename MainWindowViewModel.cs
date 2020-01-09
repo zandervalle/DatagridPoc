6365676ac85e451993241a10d41312065e08b5d0
Alexander Valerius <a.valerius@hds-systems.com>
2020-01-09 13:47:43 +0100

    Initail Commit :fire:

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace DatagridPoc
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<MyEntity> _myCollection;
+        public ObservableCollection<MyEntity> MyCollection
+        {
+            get => _myCollection;
+            set
+            {
+                _myCollection = value;
+                OnPropertyChanged(nameof(MyCollection));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            MyCollection = new ObservableCollection<MyEntity>
+            {
+                new MyEntity { MyEntityId = 1, Description = "Some Description", Value = 74.28 },
+                new MyEntity { MyEntityId = 2, Description = "Some more Description", Value = 32.32 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MyEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatagridPoc
+{
+    public class MyEntity
+    {
+        public int MyEntityId { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+    }
+}
